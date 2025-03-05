b9750d6339a13379e65b0ef70b801d35db8fe2d4
Athul Kesav <me.nd.mself@gmail.com>
2025-03-06 03:02:18 +0530

    UI improved

--- frontend/ai-therapist/src/app/page.tsx
+++ frontend/ai-therapist/src/app/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col items-center justify-around h-screen p-7 bg-gray-950">
-      <h1 className="text-4xl font-serif">AI Therapist</h1>
-      <p className="text-lg font-serif text-center">
-        An AI-powered chatbot that listens to you and provides emotional
-        support.
-      </p>
+    <div className="flex flex-col items-center justify-around h-screen p-7">
+      <div className="w-screen h-screen absolute z-0 ">
+        <Image src="/background.jpg" alt="background" layout="fill" objectFit="cover" className="" />
+      </div>
+      <div className="text-4xl top-1/2 sm:text-6xl text-center font-serif text-white z-0 mix-blend-difference ">
+        EmpathAIse
+        <p className="text-sm sm:text-lg font-serif text-center text-white">
+          An AI-powered companion that listens to you <br/> and provides emotional
+          support.
+        </p>
+      </div>
       <button
-        className="px-4 py-2 mt-4 text-white bg-blue-500 rounded-md active:bg-blue-300 transition-all cursor-pointer"
+        className="absolute font-semibold text-2xl font-sans bottom-7 px-5 py-2 text-black border border-transparent hover:border-black hover:text-black  bg-[#D7C5F9] hover:bg-[#D7C5F970] rounded-md active:bg-[#D7C5F9] transition-all duration-200 cursor-pointer"
         onClick={() => {
           router.push("/dude");
         }}
