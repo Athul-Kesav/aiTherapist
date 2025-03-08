2878eff3c5574ddf4f4fa7e5dde0842303dcdd73
Athul Kesav <me.nd.mself@gmail.com>
2025-03-08 14:40:53 +0530

    added a pipeline to an LLM (mistral-7b)

--- frontend/ai-therapist/src/app/api/get-video/route.ts
+++ frontend/ai-therapist/src/app/api/get-video/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
+import axios from "axios";
+
 export async function POST(request: Request): Promise<NextResponse> {
 
   console.log("Request received")
@@ -22,6 +24,7 @@ export async function POST(request: Request): Promise<NextResponse> {
 
     console.log("Sending data to other endpoints")
     // Prepare a FormData for the /analyze-audio endpoint.
+    
     const audioFormData = new FormData();
     audioFormData.append("file", new Blob([videoBuffer], { type: videoFile.type }), videoFile.name);
     const audioResponse = await fetch("http://localhost:5000/analyze-audio", {
@@ -39,12 +42,29 @@ export async function POST(request: Request): Promise<NextResponse> {
     });
     const videoData = await videoResponse.json();
 
-    console.log(videoData)
-    console.log(audioData)
-    return NextResponse.json({
+
+    const processorResponse = {
       message: "Video processed and sent successfully",
       audioResponse: audioData,
       videoResponse: videoData,
+    }
+
+    console.log(processorResponse)
+
+    const response = await axios.post("http://localhost:11434/api/generate", {
+      model:"mistral",
+      prompt:`The user is in a ${processorResponse.videoResponse.mood} mood. The voice analysis is max_pitch : ${processorResponse.audioResponse.max_pitch}, min_pitch : ${processorResponse.audioResponse.min_pitch}, average_intensity : ${processorResponse.audioResponse.average_intensity}, transcript : ${processorResponse.audioResponse.transcript}, sentiment analysis : label - ${processorResponse.audioResponse.sentiment.label}, score - ${processorResponse.audioResponse.sentiment.score}.
+      Generate a human-like response to the user's mood.`,
+      max_tokens: 100,
+      temperature: 0.9,
+      top_p: 1,
+      stream: false
+    })
+
+    console.log(response.data.response);
+
+    return NextResponse.json({
+      "response": response.data.response
     });
   } catch (error) {
     console.error("Error processing video:", error);
